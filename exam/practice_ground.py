0e1507421afa5d8643d41fec8922baf9dff0d55c
Lea <muehlberg.lea@gmail.com>
2026-06-03 13:23:05 +0200

    revising

---
+++ exam/practice_ground.py
@@ -0,0 +1,11 @@
+# insertion sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        current_value = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > current_value:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = current_value
+    return arr
+print(insertion_sort([2, 23, 12, 47, 3]))
