c32e5f422cade2e19c672ecab8f420cbc540f171
Lea <muehlberg.lea@gmail.com>
2026-06-05 12:33:57 +0200

    revising

--- exam/practice_ground.py
+++ exam/practice_ground.py
@@ -14,11 +14,23 @@ print(insertion_sort([3, 23, 5, 19, 21, 7]))
 
 # selection sort
 def selection_sort(arr):
-    for i in range(len(arr) -  1):
+    for i in range(len(arr) - 1):
         min_index = i
-        for j  in range(i + 1, len(arr)):
+        for j in range(i + 1, len(arr)):
             if arr[j] < arr[min_index]:
                 min_index = j
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 print(selection_sort([3, 23, 5, 19, 21, 7]))
+
+# bubble sort
+def bubble_sort(arr):
+    changed = True
+    while changed:
+        changed = False
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                changed = True
+    return arr
+print(bubble_sort([3, 23, 5, 19, 21, 7]))
